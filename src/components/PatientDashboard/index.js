a7456ee5379ef1c0f5ac602b2a8ba35a57d20fe6
Tommy <tommykindle83@gmail.com>
2019-08-27 12:20:17 -0700

    Ready to pull changes in from Login Form

--- src/components/PatientDashboard/index.js
+++ src/components/PatientDashboard/index.js
@@ -1,11 +1,22 @@
-import React from 'react'
+
 import NavHeader from '../PatientHeader/index.js'
-import axiosWithAuth from '../../utils'
+import { axiosWithAuth } from '../../utils'
+import React, { useState, useEffect } from 'react'
+
 
 
 const PatientDashboard = () => {
 
-
+    const [getPaitent, setPaient] = useState([])
+    useEffect(() => {
+        axiosWithAuth().get(`https://rcm-immunization-tracker.herokuapp.com/guardian/${guardianid}`)
+            .then(res => {
+                console.log('guardianid', res)
+            })
+            .catch(err => {
+                console.log('We did this wrong', err.response)
+            })
+    })
     return (
         <>
             <NavHeader />
