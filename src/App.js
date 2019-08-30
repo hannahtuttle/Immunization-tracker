5e08840e34b9b6f456cb8dedb34eeea21380db62
Tommy <tommykindle83@gmail.com>
2019-08-29 20:35:06 -0700

    re-added scss files

--- src/App.js
+++ src/App.js
@@ -21,34 +21,34 @@ function App() {
 
   return (
     <Router>
-     
-        <div className="App">
-          <ul>
-            <li>
-              <Link to='/'>Log In</Link>
-            </li>
-            <li>
-              <Link to='/SignUp'>SingUp</Link>
-            </li>
-            <li>
-              <Link to='/DoctorSignUp'>Doctor Sign Up</Link>
-            </li>
-            <Link to='/DoctorLogin' >Doctor Login</Link>
-          </ul>
-          <Route exact path='/immunizations' component={Immunizations} />
-          <Route exact path='/' component={Login} />
-          <Route exact path='/SignUp' component={SignUp} />
-          <Route exact path='/DoctorSignUp' component={DoctorSignUp} />
-          <Route exact path='/DoctorLogin' component={DoctorLogin} />
-          <Route exact path='/PatientOnboarding' component={PatientOnboarding} />
+
+      <div className="App">
+        <ul>
+          <li>
+            <Link to='/'>Log In</Link>
+          </li>
+          <li>
+            <Link to='/SignUp'>SingUp</Link>
+          </li>
+          <li>
+            <Link to='/DoctorSignUp'>Doctor Sign Up</Link>
+          </li>
+          <Link to='/DoctorLogin' >Doctor Login</Link>
+        </ul>
+        <Route exact path='/immunizations' component={Immunizations} />
+        <Route exact path='/' component={Login} />
+        <Route exact path='/SignUp' component={SignUp} />
+        <Route exact path='/DoctorSignUp' component={DoctorSignUp} />
+        <Route exact path='/DoctorLogin' component={DoctorLogin} />
+        {/* <Route exact path='/PatientOnboarding' component={PatientOnboarding} />
           <Route exact path='/PatientDashboard' component={PatientDashboard} />
           <Route exact path='/DoctorDashboard' component={DoctorDashboard} />
-          <Route exact path='/DoctorOnboarding' component={DoctorOnboarding} />
-          {/* <PrivateRoute exact path='/PatientOnboarding' component={PatientOnboarding} />
-          <PrivateRoute exact path='/PatientDashboard' component={PatientDashboard} />
-          <PrivateRoute exact path='/DoctorDashboard' component={DoctorDashboard} />
-          <PrivateRoute exact path='/DoctorOnboarding' component={DoctorOnboarding} /> */}
-        </div>
+          <Route exact path='/DoctorOnboarding' component={DoctorOnboarding} /> */}
+        <PrivateRoute exact path='/PatientOnboarding' component={PatientOnboarding} />
+        <PrivateRoute exact path='/PatientDashboard' component={PatientDashboard} />
+        <PrivateRoute exact path='/DoctorDashboard' component={DoctorDashboard} />
+        <PrivateRoute exact path='/DoctorOnboarding' component={DoctorOnboarding} />
+      </div>
     </Router>
   );
 }
